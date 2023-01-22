1f7c8f551c2ff39e4595603e7e61cfb34b25a974
Dawid <zgredzik987@gmail.com>
2023-01-22 15:28:36 +0100

    day 2

---
+++ 2015/2/main.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+from re import findall
+
+f = open("input.txt", "r").read().split()
+
+p = 0
+r = 0
+
+for _ in f:
+    dim = [int(x) for x in findall('(\d+)', _)]
+    # 2*l*w + 2*w*h + 2*h*l
+    dim.sort()
+    p += 2*dim[0]*dim[1] + 2*dim[1]*dim[2] + 2*dim[2]*dim[0] + dim[0]*dim[1]
+
+print(p)
+
+for _ in f:
+    dim = [int(x) for x in findall('(\d+)', _)]
+    # 2*l*w + 2*w*h + 2*h*l
+    dim.sort()
+    r += 2*dim[0] + 2*dim[1] + dim[0]*dim[1]*dim[2]
+
+print(r)
